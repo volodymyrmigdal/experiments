71fbaac932a38d32ad3e51672eb060739be98d58
vmigdal <volodymyr.migdal@gmail.com>
2018-11-14 13:42:49 +0200

    test

--- sample/memory.js
+++ sample/memory.js
@@ -1,2 +1,11 @@
+
+if( typeof module !== 'undefined' )
+require( 'wFiles' )
+
+var _ = wTools;
+
+_.fileProvider.fileWrite( _.path.join( __dirname, 'file' ), 'abc' );
+
 var a = [];
 while( 1 ) a.push( new Date() );
+
--- sample/timeStampsExperimet.js
+++ sample/timeStampsExperimet.js
@@ -4,9 +4,15 @@ require( 'wFiles' )
 
 var _ = wTools;
 
-var time = new Date( Date.now() );
-_.fileProvider.fileTimeSet( __filename, time, time );
-var stat = _.fileProvider.fileStat( __filename );
-console.log( time.getTime() )
-console.log( stat.mtime.getTime() )
-console.log( stat.mtime.getTime() - time.getTime() )
+// var time = new Date( Date.now() );
+// _.fileProvider.fileTimeSet( __filename, time, time );
+// var stat = _.fileProvider.fileStat( __filename );
+// console.log( time.getTime() )
+// console.log( stat.mtime.getTime() )
+// console.log( stat.mtime.getTime() - time.getTime() )
+
+// console.log( process.env.A )
+
+var stat = _.fileProvider.fileStat( _.path.join( __dirname, 'file' ) );
+
+console.log( stat );
