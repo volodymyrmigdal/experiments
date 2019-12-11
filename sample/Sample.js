facff571eb1e89c703cda8c39418e5c81208f9e7
Volodymyr Migdal <volodymyr.migdal@gmail.com>
2019-12-11 22:05:40 +0200

    experiment

---
+++ sample/Sample.js
@@ -0,0 +1,16 @@
+
+if( typeof module !== 'undefined' )
+require( 'wConsequence' );
+var _ = wTools;
+_.Consequence.UnhandledTimeOut = 1;
+var t1 = _.time.now();
+var con = new _.Consequence().error( 'x' );
+var fs = require('fs')
+fs.readFileSync( __filename )
+con.catch( err => 
+{ 
+  var t2 = _.time.now();
+  _.errAttend( err );
+  console.log( 'Error attended in:', t2 - t1 )
+  return null;
+})
